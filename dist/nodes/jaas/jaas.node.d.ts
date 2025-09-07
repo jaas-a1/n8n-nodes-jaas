1b903a66c7342a23664cfcee5a2875d41a1fc894
hlardiez <hlardiez@msn.com>
2025-09-07 12:29:25 -0400

    07092025 1230

--- dist/nodes/jaas/jaas.node.d.ts
+++
@@ -1,6 +0,0 @@
-import { IExecuteFunctions, INodeExecutionData, INodeType, INodeTypeDescription } from 'n8n-workflow';
-declare class Jaas implements INodeType {
-    description: INodeTypeDescription;
-    execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]>;
-}
-export { Jaas };
